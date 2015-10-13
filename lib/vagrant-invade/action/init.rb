882ba13ddbfc5d0ab5db4409883e5ee54a4277ca
Lennart Stein <lennart.stein@innogames.com>
2015-10-13 10:49:30 +0200

    New Feature: Support for alternative configuration files
    You are able to „overwrite“ the default invade.yml.dist file with your
    own.
    
    Just create a „invade.yml.dist“ - file and place it at:
    „~/.invade/invade.yml.dist“.
    
    You also able to create a project related default file by placing a
    „invade.yml.dist“ at the projects root directory. It could be shared
    with other users of a project by committing it to the repository.

--- lib/vagrant-invade/action/init.rb
+++ lib/vagrant-invade/action/init.rb
@@ -18,45 +18,46 @@ module VagrantPlugins
           if !invade_config_exists || @env[:invade_command_init_force]
             write_invade_config
           else
-            @env[:ui].error "[Invade] A 'invade.yml' file already exists. Use '--force' to replace file."
+            @env[:ui].error "[Invade] 'invade.yml' file already exists. Use '--force' to replace file."
           end
 
           @app.call(env)
         end
 
         def invade_config_exists
-          current_root_path = Dir.pwd
-          invade_config_file =  "#{@dir}/invade.yml"
-          if File.exist?(invade_config_file)
-            return true
-          end
-
-          false
+          File.exist?("#{@dir}/invade.yml")
         end
 
         def invade_template_exists
-          template_file_path = "#{@dir}/invade.yml.dist"
-          if File.exist?(template_file_path)
-            return true
-          end
-
-          false
+          File.exist?("#{@dir}/invade.yml.dist")
         end
 
+        def invade_user_customized_config_exists
+          File.exist?(File.expand_path('~/.invade/invade.yml.dist'))
+        end
+
+        # Writes configuration file 'invade.yml'
+        #   if template file in project folder does exist it copies it#
+        #   if template file in project folder doesn't exist and a '~/.invade/invade.yml.dist' file exists it copies it
+        #   if template file in project folder doesn't exist it uses plugins default file
         def write_invade_config
-          current_root_path = Dir.pwd
-          config_file_path = "#{@dir}/invade.yml"
+          invade_config_file = "#{@dir}/invade.yml"
 
           if invade_template_exists
             template_file_path = "#{@dir}/invade.yml.dist"
-            FileUtils.cp(template_file_path, config_file_path)
-            @env[:ui].success "[Invade] Copy of template 'invade.yml.dist' created successfully. Please make your changes."
+            FileUtils.cp(template_file_path, invade_config_file)
+            @env[:ui].success "[Invade] Project template file '#{template_file_path}' was found!"
+          elsif invade_user_customized_config_exists && !invade_template_exists
+            FileUtils.cp(File.expand_path('~/.invade/invade.yml.dist'), invade_config_file)
+            @env[:ui].success "[Invade] User customized template file '~/.invade/invade.yml.dist' was found!"
           else
             plugin_root_path = File.expand_path('../../../../', __FILE__)
-            default_config_file_path = plugin_root_path + '/invade.yml.dist'
-            FileUtils.cp(default_config_file_path, config_file_path)
-            @env[:ui].success "[Invade] Copy of default 'invade.yml.dist' created successfully. Please make your changes."
+            default_invade_config_file = plugin_root_path + '/invade.yml.dist'
+            FileUtils.cp(default_invade_config_file, invade_config_file)
+            @env[:ui].warn '[Invade] Project template file or customized user configuration file NOT found!'
           end
+
+          @env[:ui].success '[Invade] \'invade.yml\' created successfully. Please make your changes.'
         end
       end
 
