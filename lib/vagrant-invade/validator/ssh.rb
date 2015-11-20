9deea0ca13eee9fb827b6736084d6123abf9a72a
Jost Künzel <jost.kuenzel@innogames.com>
2015-11-20 10:00:32 +0100

    Removes support for ssh.path and ssh.enabled as it is not part of ssh setting: https://docs.vagrantup.com/v2/vagrantfile/ssh_settings.html

--- lib/vagrant-invade/validator/ssh.rb
+++ lib/vagrant-invade/validator/ssh.rb
@@ -7,9 +7,7 @@ module VagrantPlugins
         attr_accessor :env, :ssh
 
         DEFAULT = {
-          'forward_agent' => true,
-          'enabled' => true,
-          'path' => '~/.ssh/'
+          'forward_agent' => true
         }
 
         def initialize(env, ssh)
@@ -19,16 +17,6 @@ module VagrantPlugins
 
         def validate
           return DEFAULT unless @ssh
-
-          # ENABLED
-          @ssh['enabled'] = Validator.validate_boolean(
-            @ssh['enabled'], 'enabled', DEFAULT['enabled']
-          )
-
-          # PATH TO HOST SSH FOLDER
-          @ssh['path'] = Validator.validate_string(
-            @ssh['path'], 'path', DEFAULT['path']
-          )
 
           @ssh['forward_agent'] = Validator.validate_boolean(
             @ssh['forward_agent'], 'forward_agent', DEFAULT['forward_agent']
