b28236883cd09ad3bce3df11d914413725b06815
Lennart Stein <lennart.stein@sedo.com>
2017-01-19 12:06:44 +0100

    Plugin - Hostmanager: Update template

--- lib/vagrant-invade/module/plugin/hostmanager/hostmanager.rb
+++ lib/vagrant-invade/module/plugin/hostmanager/hostmanager.rb
@@ -26,6 +26,7 @@ module VagrantPlugins
               machine_name = @machine_name
 
               # Values for hostmanager section
+              puts @hostmanager_data['enabled']
               enabled = @hostmanager_data['enabled']
               aliases = @hostmanager_data['aliases']
 
