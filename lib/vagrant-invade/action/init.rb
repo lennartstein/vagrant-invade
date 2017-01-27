b3c9c33287af021c196627eaabcfbe72a7a97051
Lennart Stein <lennart.stein@sedo.com>
2017-01-27 14:20:24 +0100

    Command - Init: Update info output

--- lib/vagrant-invade/action/init.rb
+++ lib/vagrant-invade/action/init.rb
@@ -46,18 +46,23 @@ module VagrantPlugins
           if invade_template_exists
             template_file_path = "#{@dir}/invade.yml.dist"
             FileUtils.cp(template_file_path, invade_config_file)
-            @env[:ui].success "[Invade] Project template file '#{template_file_path}' was found!"
+            @env[:ui].success "[Invade] Using local template file '#{template_file_path}'."
           elsif invade_user_customized_config_exists && !invade_template_exists
             FileUtils.cp(File.expand_path('~/.invade/invade.yml.dist'), invade_config_file)
-            @env[:ui].success "[Invade] User customized template file '~/.invade/invade.yml.dist' was found!"
+            @env[:ui].success "[Invade] Using global template file '~/.invade/invade.yml.dist'."
           else
             plugin_root_path = File.expand_path('../../../..', __FILE__)
             default_invade_config_file = plugin_root_path + '/invade-default.yml'
+            template_file_path = "#{@dir}/invade.yml.dist"
             FileUtils.cp(default_invade_config_file, invade_config_file)
-            @env[:ui].warn '[Invade] Project template file or customized user configuration file NOT found!'
+            @env[:ui].warn '[Invade] Global user template file \'~/.invade/invade.yml.dist\' not found.'
+            @env[:ui].warn "[Invade] Local template file './invade.yml.dist' not found."
+            @env[:ui].warn '[Invade] ------------------------------------------------------------------'
+            @env[:ui].warn "[Invade] Save a template file to it's location to use it as default."
           end
 
-          @env[:ui].success '[Invade] \'invade.yml\' created successfully. Please make your changes.'
+          @env[:ui].success ""
+          @env[:ui].success '[Invade] Default \'invade.yml\' created successfully. Please make your changes.'
         end
       end
 
